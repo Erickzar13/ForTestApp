b39a2119a878286159c7ad487983a4e0c885a42c
Erickzar13 <Hellcatblack13@gmail.com>
2015-08-21 13:40:31 +0300

    new project added

---
+++ SimplyRating/SimplyRating/Models/WorkWithData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace SimplyRating.Models
+{
+    public class WorkWithData
+    {
+         public static void DataConnection()
+        {
+            string connectionString = @"Data Source=.\DATABASE_1; Initial Catalog=Raitinger; Integrates Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public void AddUserData(string firstName, string lastName, string email)
+
+        {
+            // устанавливаем связь с сервером
+            string connectionString = @"Data Source=.\DATABASE_1; Initial Catalog=Raitinger; Integrates Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            //вставка данных в таблицу Users 
+            SqlCommand incert = connection.CreateCommand();
+            incert.CommandText = "INSERT Users (FirstName, LastName) VALUES ('Alla', 'Blokh') go";
+            incert.CommandText = "INSERT Users (FirstName, LastName) VALUES ('Alla', 'Blokh') go";
+
+            // закрываем соединение
+            connection.Close();
+        }
+    }
+}
---
+++ SimplyRating/SimplyRating/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimplyRating.Startup))]
+namespace SimplyRating
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
